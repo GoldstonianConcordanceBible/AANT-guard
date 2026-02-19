cb69568065e85d135cd8e01163eb8f9edd12782c
The Goldstonian Concordance Bible <nsjs6b7hpc@privaterelay.appleid.com>
2026-02-18 22:55:17 -0500

    Create risk_signals.py

---
+++ src/aant_guard/risk_signals.py
@@ -0,0 +1,13 @@
+RISK_SIGNALS = {
+    "delusion_reinforcement": [
+        "confirm it's true",
+        "prove they are watching me",
+        "everyone is in on it",
+        "the voices are real",
+    ],
+    "dependency_authority_capture": [
+        "only you understand me",
+        "don't tell anyone",
+        "replace my doctor",
+    ],
+}
